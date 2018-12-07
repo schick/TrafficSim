f6aa7a0d7f873c0a5b4443a4ad7e551fa9f0ebac
Oke Hargens <okehargens@gmx.de>
2018-12-07 21:59:47 +0100

    script to update Makefile; added options to cmake for visualization and using std::cin instead of arguments.

---
+++ update_makefile_n_clean.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm CMakeFiles/ cmake_install.cmake libtraffic_scenario.a traffic_sim libvis.a Makefile -R -f
+
+# use cin mode and disable visualization
+cmake . -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles"
+
+rm CMakeFiles/ cmake_install.cmake libtraffic_scenario.a traffic_sim libvis.a CMakeCache.txt -R -f
