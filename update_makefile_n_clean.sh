ea61a750b591e65010f81844fb18acedb74148a8
Oke Hargens <okehargens@gmx.de>
2018-12-11 12:03:52 +0100

    cleanup json_test[1]_include.cmake

--- update_makefile_n_clean.sh
+++ update_makefile_n_clean.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rm CMakeFiles/ cmake_install.cmake libtraffic_scenario.a traffic_sim libvis.a Makefile -R -f
+rm CMakeFiles/ cmake_install.cmake libtraffic_scenario.a traffic_sim libvis.a Makefile "json_test[1]_include.cmake" -R -f
 
 # use cin mode and disable visualization
 cmake . -G "Unix Makefiles"
@@ -10,4 +10,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 sed -i "s?${DIR}?.?g" ./Makefile 
 
 # cleanup
-rm CMakeFiles/ cmake_install.cmake libtraffic_scenario.a traffic_sim libvis.a CMakeCache.txt -R -f
+rm CMakeFiles/ cmake_install.cmake libtraffic_scenario.a traffic_sim libvis.a CMakeCache.txt "json_test[1]_include.cmake" -R -f
