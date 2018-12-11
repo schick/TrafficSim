119bb5957a421424d2837710c460eff43580e16a
Oke Hargens <okehargens@gmx.de>
2018-12-11 12:14:13 +0100

    updated update_makefile_n_cleanup.sh

--- update_makefile_n_clean.sh
+++ update_makefile_n_clean.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
-rm CMakeFiles/ cmake_install.cmake libtraffic_scenario.a traffic_sim libvis.a Makefile "json_test[1]_include.cmake" -R -f
+# directory of this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# change into repository directory
+cd ${DIR}
+
+# files to delete for cleanup
+CLEANUP_FILES="CMakeFiles/ cmake_install.cmake libtraffic_scenario.a traffic_sim libvis.a
+        json_test[1]_include.cmake json_test[1]_tests.cmake"
+
+# cleanup caches and old builds
+rm ${CLEANUP_FILES} -R -f
 
 # use cin mode and disable visualization
 cmake . -G "Unix Makefiles"
 
 # replace absolute paths
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 sed -i "s?${DIR}?.?g" ./Makefile 
 
-# cleanup
-rm CMakeFiles/ cmake_install.cmake libtraffic_scenario.a traffic_sim libvis.a CMakeCache.txt "json_test[1]_include.cmake" -R -f
+# cleanup - we only want to keep the Makefile
+rm ${CLEANUP_FILES} -R -f
