8334688e57ca66ee623563b93eef8bd93d9f204a
schick <maxi.f.schick@gmail.com>
2019-01-10 17:15:18 +0100

    Removed new and delete while finding neighbors.

--- scenario_ref/src/NeighborFinder.cpp
+++ scenario_ref/src/NeighborFinder.cpp
@@ -5,29 +5,28 @@
 #include "NeighborFinder.h"
 
 Lane::NeighboringObjects NeighborFinder::getNeighboringObjects(Lane *lane, TrafficObject *trafficObject) {
-//create empty neighboringObjects struct
+    //create empty neighboringObjects struct
     auto neighboringObjects = Lane::NeighboringObjects();
 
-//if null reference return empty struct
+    //if null reference return empty struct
     if (lane == nullptr)
         return neighboringObjects;
 
-    auto trafficObjects = lane->mTrafficObjects;
-//if lane is empty return empty struct
-    if (trafficObjects.size() == 0)
+    //if lane is empty return empty struct
+    if (lane->mTrafficObjects.size() == 0)
         return Lane::NeighboringObjects();
 
-    auto it = std::lower_bound(trafficObjects.begin(), trafficObjects.end(), trafficObject, TrafficObject::Cmp());
+    auto it = std::lower_bound(lane->mTrafficObjects.begin(), lane->mTrafficObjects.end(), trafficObject, TrafficObject::Cmp());
     Lane::NeighboringObjects result;
 
-    if (it != trafficObjects.begin())
+    if (it != lane->mTrafficObjects.begin())
         result.back = *(it - 1);
 
-    if (trafficObjects.end() == it || *it != trafficObject) {
-        if (it != trafficObjects.end())
+    if (lane->mTrafficObjects.end() == it || *it != trafficObject) {
+        if (it != lane->mTrafficObjects.end())
             result.front = *it;
     } else {
-        if (it + 1 != trafficObjects.end())
+        if (it + 1 != lane->mTrafficObjects.end())
             result.front = *(it + 1);
     }
     return result;
