9e1d29c224232fa3e2abb8bc84c31268d5443d5c
Oke Hargens <okehargens@gmx.de>
2019-01-21 13:58:44 +0100

    Force at least one signal.

--- scenario_ref/src/optimization/DistributionOptimizer.cpp
+++ scenario_ref/src/optimization/DistributionOptimizer.cpp
@@ -25,18 +25,31 @@ void _randomInitialization(std::vector<std::array<double, 4>> &incoming_counts, 
     for(auto &junction : scenario.junctions) {
         junction.signals.resize(0);
         std::vector signal_idxs = {0, 1, 2, 3};
-        double &max_metric = *std::max_element(incoming_counts[junction.id].begin(), incoming_counts[junction.id].end());
+        double *max_metric = std::max_element(incoming_counts[junction.id].begin(), incoming_counts[junction.id].end());
         size_t mean_max_duration = range_random(9, 15);
+
         for(size_t idx = 0; idx < 4; idx += 1) {
             size_t i = range_random(signal_idxs.size());
             size_t signal_idx = signal_idxs[i];
             signal_idxs.erase(signal_idxs.begin() + i);
-            std::normal_distribution<double> distribution(incoming_counts[junction.id][signal_idx] / max_metric * mean_max_duration, 1);
+            std::normal_distribution<double> distribution(incoming_counts[junction.id][signal_idx] / *max_metric * mean_max_duration, 1);
             auto duration = (size_t ) std::abs(std::round(distribution(generator)));
             if (duration < 5 && duration > 2) duration = 5;
             if (duration <= 2) continue;
             junction.signals.emplace_back(duration, (Junction::Direction ) signal_idx);
+            if(duration < 5) throw std::runtime_error("Duration is too small. " + std::to_string(duration));
         }
+        if(junction.signals.size() == 0) {
+            if(*max_metric > 0) {
+                size_t max_idx = max_metric - incoming_counts[junction.id].begin();
+                junction.signals.emplace_back(5, (Junction::Direction ) std::min((size_t) 3, max_idx));
+            } else {
+                size_t max_idx = 0;
+                for(size_t i=0; i < 4; i++) if (junction.incoming[i] != nullptr) max_idx = i;
+                junction.signals.emplace_back(5, (Junction::Direction ) max_idx);
+            }
+        }
+        assert(!junction.signals.empty());
     }
     scenario.initJunctions();
 }
